4012213389156493691491ae98edcccdffb4af6b
AipzzzX <aipzzzx@qq.com>
2024-09-19 11:27:23 +0800

    6.TodoList案例_底部统计_1

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -3,7 +3,7 @@
     <label>
       <input type="checkbox" />
     </label>
-    <span> <span>已完成0</span> / 全部2 </span>
+    <span> <span>已完成 {{doneTotel}}</span> / 总共 {{ todos.length }} </span>
     <button class="btn btn-danger">清除已完成任务</button>
   </div>
 </template>
@@ -11,6 +11,15 @@
 <script>
 export default {
   name: "MyFooter",
+  props: ["todos"],
+  computed: {
+    doneTotel() {
+      return this.todos.reduce(
+        (pre, current) => pre + (current.done ? 1 : 0),
+        0
+      );
+    },
+  },
 };
 </script>
 
